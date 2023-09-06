7149f84140af6db2d8a1b58dfd22ed4aa70e0525
Pedro29ph <phsouza64@gmail.com>
2023-09-06 18:36:56 -0300

    Update t5.py

--- t5.py
+++ t5.py
@@ -3,12 +3,12 @@ print('seja bem-vindo ao programa de tranformaão de escala de temperatura')
 escala= str(input('digite C para celso, F para fahrenheit e K para Kelvin'))
 x= float(input('digite o valor que vai ser convertido'))
 
-if escala== 'C':
+if escala== 'C' or escala== 'c':
     x1=x+273
     x2=((x*1.8)+32)
     print(f'valor em kelvin:{x1}')
     print(f'valor em fahrenheit:{x2}')
-elif escala== 'F':
+elif escala== 'F' or escala== 'f':
     x1= (x-32)/1.8
     x2=(((x-32)*0.555555556)+273)
     print(f'valor em celso:{x1}')
