0ccfb3d5ee4bb8df9cd26784642a950a42459f54
Pedro29ph <phsouza64@gmail.com>
2023-09-11 17:14:31 -0300

    Update t5.py

--- t5.py
+++ t5.py
@@ -6,15 +6,15 @@ x= float(input('digite o valor que vai ser convertido'))
 if escala== 'C' or escala== 'c':
     x1=x+273
     x2=((x*1.8)+32)
-    print(f'valor em kelvin:{x1}')
-    print(f'valor em fahrenheit:{x2}')
+    print(f'valor em kelvin: {x1}')
+    print(f'valor em fahrenheit: {x2}')
 elif escala== 'F' or escala== 'f':
     x1= (x-32)/1.8
     x2=(((x-32)*0.555555556)+273)
-    print(f'valor em celso:{x1}')
-    print(f'valor em kelvin:{x2}')
+    print(f'valor em celso: {x1}')
+    print(f'valor em kelvin: {x2}')
 else:
     x1=x-273
     x2= (((x-273)*1.8)+32)
-    print(f'valor em celso:{x1}')
-    print(f'valor em fahrenheit:{x2}')
+    print(f'valor em celso: {x1}')
+    print(f'valor em fahrenheit: {x2}')
